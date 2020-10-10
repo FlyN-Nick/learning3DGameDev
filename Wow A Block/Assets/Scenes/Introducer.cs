e14965fef1cb68a386bdc3b9b07de05f4cb68eb3
FlyN Nick <nickassader@gmail.com>
2020-10-09 22:35:48 -0700

    wOw I made a car

---
+++ Wow A Block/Assets/Scenes/Introducer.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// I may have collapsed the class quite a bit...
+
+public class Introducer : MonoBehaviour { void Start() { print("First time writing a print statement in C# since 2019."); } }
