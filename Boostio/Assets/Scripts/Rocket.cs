6482590651d371185a3286f9f0c4b42d7c8c655c
FlyN Nick <nickassader@gmail.com>
2020-10-18 14:08:26 -0700

    The rocket now can be thrusted.

---
+++ Boostio/Assets/Scripts/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    void Start() { rigidbody = GetComponent<Rigidbody>(); }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //print("Thrusting");
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            //print("Left");
+            // TODO: make rocket rotate left 
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //print("Right");
+            // TODO: make rocket rotate right 
+        }
+    }
+}
