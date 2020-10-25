dee2df52a1f9b63291f8d9acb55c741a6b43ea32
FlyN Nick <nickassader@gmail.com>
2020-10-25 15:29:14 -0700

    Collision detection + bug squashin.

--- Boostio/Assets/Scripts/Rocket.cs
+++ Boostio/Assets/Scripts/Rocket.cs
@@ -4,40 +4,71 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    private Rigidbody rigidbody;
+    [SerializeField] private float rcsThrust = 100f;
+    [SerializeField] private float mainThrust = 100f;
+
+    private Rigidbody rigidBody;
     private AudioSource audioSource;
 
-    void Start()
+    private void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
 
-    void Update()
+    private void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // do nothing
+                print("Chill."); // TODO: remove
+                break;
+            case "Fuel":
+                // TODO: refuel
+                print("Fuel."); // TODO: remove
+                break;
+            default:
+                // TODO: you died
+                print("F."); // TODO: remove
+                break;
+        }
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             // thrust rocket 
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up*mainThrust);
             // play thrust sfx
             if (!audioSource.isPlaying) { audioSource.Play(); }
         }
         else if (audioSource.isPlaying) { audioSource.Stop(); }
+    }
 
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        float rotSpeed = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
         {
-            // rotate left 
-            transform.Rotate(Vector3.forward);
+            // rotate left
+            transform.Rotate(Vector3.forward*rotSpeed);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             // rotate right 
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(Vector3.back*rotSpeed);
         }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation 
     }
 }
