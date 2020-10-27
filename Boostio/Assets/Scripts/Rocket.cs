08f05c39704a7d1fd64a76f4f13eb8d0f5ddef0c
FlyN Nick <nickassader@gmail.com>
2020-10-27 00:06:04 -0700

    Now the level changes (not properly yet tho)

--- Boostio/Assets/Scripts/Rocket.cs
+++ Boostio/Assets/Scripts/Rocket.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class Rocket : MonoBehaviour
     private Rigidbody rigidBody;
     private AudioSource audioSource;
 
+    private enum State { alive, dead, transcending }
+    State state = State.alive;
+
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
@@ -18,29 +22,49 @@ public class Rocket : MonoBehaviour
 
     private void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.alive)
+        {
+            Thrust();
+            Rotate();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (state != State.alive) { return; }
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 // do nothing
-                print("Chill."); // TODO: remove
+                // TODO: switch to if else?
                 break;
             case "Fuel":
                 // TODO: refuel
-                print("Fuel."); // TODO: remove
+                break;
+            case "Finish":
+                // TODO: next level or level screen?
+                // TODO: play next level sound?
+                state = State.transcending;
+                Invoke("LoadNextLevel", 1f);
                 break;
             default:
-                // TODO: you died
-                print("F."); // TODO: remove
+                // TODO: first level or level screen?
+                // TODO: stop sound on death?
+                // TODO: play dying sound?
+                state = State.dead;
+                Invoke("LoadFirstLevel", 1f);
                 break;
         }
     }
 
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1); // TODO: make this work for more than 2 levels
+    }
+
+    void LoadFirstLevel() { SceneManager.LoadScene(0);  }
+    
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
